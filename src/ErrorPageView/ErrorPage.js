b15a4032ef55461649a447b6e36b298074805315
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:50:28 -0500

    Add files via upload

--- src/ErrorPageView/ErrorPage.js
+++ src/ErrorPageView/ErrorPage.js
@@ -1,1 +1,22 @@
+// CSS
+import './ErrorPage.css';
 
+// React
+import React from 'react';
+
+// Needed Components
+import Header from '../Header';
+import Footer from '../Footer';
+
+
+function ErrorPage(){
+    return(
+        <>
+            <Header />
+            <p>This is the error page. Page not found... </p>
+            <Footer />
+        </>
+    );
+}
+
+export default ErrorPage;
