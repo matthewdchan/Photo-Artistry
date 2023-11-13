6fe7fa67585e9462d643c579096dde71a99e10e9
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:41:36 -0500

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// CSS
+import './Footer.css';
+
+// REACT
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer>
+            <p>About Us</p>
+            <p>Privacy Policy</p>
+            <p>Help</p>
+            <h2>PhotoArtistry</h2>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+// CSS
+import './Header.css';
+
+// React
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Needed Components
+
+function Header(props){
+    return(
+        <header>
+            <h2>PhotoArtistry</h2>            
+        </header>
+    );
+}
+
+export default Header;
