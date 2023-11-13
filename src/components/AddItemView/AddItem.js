63587a53df74cd313392bdd7ee45fa1083d3d7c0
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:48:58 -0500

    Add files via upload

--- src/components/AddItemView/AddItem.js
+++ src/components/AddItemView/AddItem.js
@@ -1,1 +1,21 @@
+// CSS
+import './AddItem.css';
 
+// React
+import React from 'react';
+
+// Needed Components
+import Header from '../Header';
+import Footer from '../Footer';
+
+function AddItem(){
+    return(
+        <>
+            <Header />
+            <p>THIS IS THE ADD ITEM PAGE</p>
+            <Footer />
+        </>
+    );
+} 
+
+export default AddItem;
