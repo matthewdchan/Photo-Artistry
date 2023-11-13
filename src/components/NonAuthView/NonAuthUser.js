475377c1fa8ba6637a990467f65b06b1ff994db1
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:47:49 -0500

    Add files via upload

--- src/components/NonAuthView/NonAuthUser.js
+++ src/components/NonAuthView/NonAuthUser.js
@@ -1,1 +1,22 @@
+// CSS
+import './NonAuthUser.css';
 
+// React
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Needed Components
+import Header from '../Header';
+import Footer from '../Footer';
+
+function NonAuthUser(){
+    return(
+        <div>
+            <Header />
+            <p> This is the NONAUTHUSER page </p>
+            <Footer />
+        </div>
+    );
+}
+
+export default NonAuthUser;
