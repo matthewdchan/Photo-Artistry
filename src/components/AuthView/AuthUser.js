772f5e16a2fe56b05564279183b187be16169220
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:46:28 -0500

    Add files via upload

--- src/components/AuthView/AuthUser.js
+++ src/components/AuthView/AuthUser.js
@@ -1,1 +1,21 @@
+// CSS
+import './AuthUser.css';
 
+// React
+import React from 'react';
+
+// Needed Components
+import Header from '../Header';
+import Footer from '../Footer';
+
+function AuthUser(){
+    return (
+        <>
+            <Header />
+            <p>THIS IS THE AUTH USER PAGE</p>
+            <Footer />
+        </>
+    );
+} 
+
+export default AuthUser;
