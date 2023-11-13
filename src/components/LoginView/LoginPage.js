7c40a3df215ad12c6cb84781c5aad1ee7276ec52
-kalvin- <63025530+kalvintn@users.noreply.github.com>
2023-11-13 01:49:38 -0500

    Add files via upload

--- src/components/LoginView/LoginPage.js
+++ src/components/LoginView/LoginPage.js
@@ -1,1 +1,21 @@
+// CSS
+import './LoginPage.css';
 
+// React
+import React from 'react';
+
+// Needed Components
+import Header from '../Header';
+import Footer from '../Footer';
+
+function LoginPage(){
+    return(
+        <>
+            <Header />
+            <p>THIS IS THE LOGIN PAGE</p>
+            <Footer />
+        </>
+    );
+}
+
+export default LoginPage;
