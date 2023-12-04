53d09519b0419e89768d5bbed21d084822ac6f2f
Adam Tropauer <apt23028@uga.edu>
2023-12-03 22:23:17 -0500

    Fixed post requests in Login.js & Signup.js, fixed schema in Users.js, email & passwords are now they're proper types instead of being type 'text'

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     username: {
         type: String,
         required: true,
+        trim: true,
     },
     password: {
         type: String,
         required: true,
+        trim: true,
+        minLength: 6
     },
  }
 );
